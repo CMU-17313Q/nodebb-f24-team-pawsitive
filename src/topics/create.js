25c382ca50d57b162cfaf24e317620ca0c5d7967
Moza Al Thani <mthani2@andrew.cmu.edu>
2024-10-25 17:08:16 +0300

    anonymous, create.js changes

--- src/topics/create.js
+++ src/topics/create.js
@@ -117,6 +117,9 @@ module.exports = function (Topics) {
 			await user.isReadyToPost(uid, data.cid);
 		}
 
+		if (data.isAnonymous) {
+			data.uid = 0;
+		}
 		const tid = await Topics.create(data);
 
 		let postData = data;
