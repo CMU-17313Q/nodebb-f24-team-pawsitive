7f68ebcd2f4421bfb18b1c87ef64a45656c13e90
Filippos <fdounis@andrew.cmu.edu>
2024-11-18 20:14:26 +0300

    Troubleshooted issues in index.js, with the llm now working fully

--- src/translate/index.js
+++ src/translate/index.js
@@ -5,8 +5,10 @@ const fetch = require('node-fetch');
 
 translatorApi.translate = async function (postData) {
 	// Edit the translator URL below
-	const TRANSLATOR_API = 'https://teampawsitive-translator-gqf3a7ejhqa3fcf4.uaenorth-01.azurewebsites.net/';
+	const TRANSLATOR_API = 'https://teampawsitive-translator-gqf3a7ejhqa3fcf4.uaenorth-01.azurewebsites.net';
 	const response = await fetch(`${TRANSLATOR_API}/?content=${postData.content}`);
 	const data = await response.json();
+	// Log the data to the console
+  console.log('Translator API Response:', data);
 	return [data.is_english, data.translated_content];
 };
